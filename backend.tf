2744c23692ef7e146db275fc322ff662490003bd
AishwaryaC <140382915+my-gitlab@users.noreply.github.com>
2023-07-25 00:14:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "sweetyhoney"
     key = "main"
-    region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    region = "ap-south-1"
+    dynamodb_table = "MyTable"
   }
 }
